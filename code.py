0e915d5bbda12f69a56b50b3c97102b146b11132
gaurav1024 <gaurav.agr100@gmail.com>
2017-06-01 16:19:52 +0530

    Correct the typo 'yoy' to 'you'.

--- code.py
+++ code.py
@@ -1,7 +1,8 @@
 import subprocess
 import os
-text_to_find = raw_input('What do yoy want to find  :  ')
-for (dirpath, dirname, filename) in os.walk('./'):
+text_to_find = raw_input('What do you want to find  :  ')
+target_directory = raw_input('Please enter the path of the target directory  :  ')
+for (dirpath, dirname, filename) in os.walk(target_directory):
 	for file in filename:
 		filepath = dirpath+file
 		output = subprocess.check_output(['file', filepath])
